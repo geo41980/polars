b7ee00ea7319fd9073ecfb1344aee13b40748ff1
Georg <141832610+geo41980@users.noreply.github.com>
2023-08-14 11:20:16 +0200

    Update polars-dataframe.py

--- polars-dataframe.py
+++ polars-dataframe.py
@@ -1,23 +1,4 @@
 import polars as pl
- 
-df = pl.DataFrame(
-    {
-        'Modell': ['iPhone X','iPhone XS','iPhone 12','iPhone 13','Samsung S11','Samsung S12','Mi A1','Mi A2'],
-        'Verkäufe': [80,170,130,205,400,30,14,8],     
-        'Unternehmen': ['Apple','Apple','Apple','Apple','Samsung','Samsung','Xiao Mi','Xiao Mi'],
-    }
-)
-#df
-#df2 = pl.DataFrame(
-#    {
-#        "0" : [1,2,3],
-#        "1" : [80,170,130],
-#    }
-#)
-#df.dtypes
-#df.columns 
-#df.select(['Modell','Unternehmen'])
-#df.select(pl.col(pl.Int64))
-df.row(1)   # erste Zeile abrufen
-#df.select('Modell')
-df.filter(pl.col('Unternehmen') == 'Apple')
+
+s = pl.Series("a", [1, 2, 3, 4, 5])
+print(s)
